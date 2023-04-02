c3139735b1f0d20e4fe4b962d2ee2486a93b4e57
n4nd0g4rc14 <128625777+n4nd0g4rc14@users.noreply.github.com>
2023-04-02 01:00:00 -0300

    Delete somatorio.js

--- somatorio.js
+++
@@ -1,9 +0,0 @@
-var n = prompt("Quantos números serão somados?");
-var sum = 0;
-
-for (var p = 1; p <= n; p++) {
-  var number = prompt("Digite um número" + p );
-  sum += Number(number);
-}
-
-console.log("A soma dos " + n + " é igual a: " + sum);
